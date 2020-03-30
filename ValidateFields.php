d18a46b2a05b26ff6edec479db06d63b421edf7d
Alexander Toshio Davila Chinen <puf.toshi@gmail.com>
2020-03-30 18:27:20 -0500

    adding ValidateFields.php

---
+++ ValidateFields.php
@@ -0,0 +1,22 @@
+<?php
+
+class ValidateFields
+{
+  private function setFields($value)
+  {
+    return $value;
+  }
+  
+  private function emptyFields($value)
+  {
+    $result = 0;
+    $value = $this->setFields($value);
+    if(empty($value))
+    {
+      $result = 'El campo no puede estar vacío,';
+    }
+    return $result;
+  }
+}
+
+?>
